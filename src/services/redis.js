2ed74dd4c62564de6fbf949e5eafe3d15621ebcf
Yazan Medanat <medanat@gmail.com>
2018-06-08 07:17:10 -0700

    Adds more redis info.

--- src/services/redis.js
+++ src/services/redis.js
@@ -1,15 +1,32 @@
-function info(redis) {
-  return new Promise((resolve, reject) => {
-    redis.dbsize((err, dbsize) => {
-      if (err) {
-        return reject(err);
-      }
+const { promisify } = require('util');
 
-      return resolve({
-        dbsize
-      });
-    });
-  });
+function parseInfoBlob(infoBlob) {
+  const sections = infoBlob.trim().split('\r\n\r\n');
+
+  return sections.reduce((acc, section) => {
+    const lines = section.split('\r\n');
+
+    acc[lines.shift().slice(2).toLowerCase()] = lines.reduce((acc, line) => {
+      const [key, value] = line.split(':');
+
+      acc[key] = value;
+
+      return acc;
+    }, {});
+
+    return acc;
+  }, {});
+}
+
+function info(redis) {
+  return Promise.all([
+    promisify(redis.dbsize).bind(redis)(),
+    promisify(redis.info).bind(redis)('all')
+  ])
+    .then(([dbsize, infoBlob]) => ({
+      dbsize,
+      ...parseInfoBlob(infoBlob)
+    }));
 }
 
 module.exports = info;
--- test/services/redis.js
+++ test/services/redis.js
@@ -5,7 +5,9 @@ describe('redis', () => {
   it('reports redis related attributes', done => {
     redis(require('redis').createClient())
       .then(data => {
-        expect(data).to.have.keys('dbsize');
+        expect(data).to.have.keys('dbsize', 'clients', 'cluster',
+          'commandstats', 'cpu', 'keyspace', 'memory', 'persistence',
+          'replication', 'server', 'stats');
         expect(data.dbsize).to.be.a('number');
 
         done();
