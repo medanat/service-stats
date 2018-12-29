aa39ce266a215220648b10831e2f4ce05aa42718
Yazan Medanat <yazan@logojoy.com>
2018-12-29 11:03:45 +0200

    Turn off daemon mode for tests.

--- test/services/pm2.js
+++ test/services/pm2.js
@@ -1,9 +1,12 @@
 const { expect } = require('chai'),
+      { custom: Pm2 } = require('pm2'),
       { pm2 } = require('../../src/services');
 
 describe('pm2', () => {
   it('reports pm2 related attributes', done => {
-    pm2(require('pm2'))
+    pm2(new Pm2({
+      daemon_mode: false
+    }))
       .then(data => {
         expect(data).to.have.keys('currentApp', 'ids', 'processes');
 
